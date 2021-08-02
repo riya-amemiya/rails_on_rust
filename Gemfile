181326b49664a208d18d33220f184b908d217b59
Riya <hiyoriorria@gmail.com>
2021-08-02 13:44:05 +0900

    first commit

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "react-rails"
 gem "ffi"
 gem "pycall"
+gem 'tailwindcss-rails'
--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,33 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
-    })
-  ]
+let environment = {
+    plugins: [
+        require('autoprefixer'),
+        require('tailwindcss'),
+        require('postcss-import'),
+        require('postcss-flexbugs-fixes'),
+        require('postcss-preset-env')({
+            autoprefixer: {
+                flexbox: 'no-2009'
+            },
+            stage: 3
+        }),
+    ]
 }
+
+// Only run PurgeCSS in production (you can also add staging here)
+if (process.env.RAILS_ENV === "production") {
+    environment.plugins.push(
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                './app/**/*.html.erb',
+                './app/helpers/**/*.rb',
+                './app/javascript/**/*.js',
+                './app/javascript/**/*.jsx',
+                './app/javascript/**/*.tsx',
+                './app/javascript/**/*.ts',
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
+    )
+}
+
+module.exports = environment
