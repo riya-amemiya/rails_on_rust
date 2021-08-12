dd1632e0110a199117f649388c48b708f2567de2
Riya <hiyoriorria@gmail.com>
2021-08-12 21:55:00 +0900

    微細な変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-//import "../css/tailwindcss.css";
-//require("stylesheets/application.scss")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
@@ -16,3 +14,4 @@ ActiveStorage.start()
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
+//= require_tree .
