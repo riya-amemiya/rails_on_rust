061214496bc6421a1551a1949f287d64ec27f105
Riya <hiyoriorria@gmail.com>
2021-08-13 14:49:28 +0900

    微細な変更

--- app/javascript/components/templates/Header.tsx
+++ app/javascript/components/templates/Header.tsx
@@ -1,10 +1,14 @@
 import { AppBar } from '@material-ui/core';
 import React from 'react';
+import styled from 'styled-components';
 const Header = () => {
+    const DIV = styled.div`
+        text-align: center;
+    `;
     return (
         <>
             <AppBar>
-                <div>header</div>
+                <DIV>header</DIV>
             </AppBar>
             ;
         </>
