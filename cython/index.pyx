b23bc8b9fd9e06113dd28a439e3c5b9020ead94c
Riya <hiyoriorria@gmail.com>
2021-08-17 14:51:36 +0900

    pythonの機能追加

--- cython/index.pyx
+++ cython/index.pyx
@@ -1,4 +1,7 @@
-cpdef c_pow(x):
+import requests
+cpdef c_pow(int x):
     return x * x;
-cpdef c_add(x):
+cpdef c_add(int x):
     return x + x;
+cpdef http_request(str x):
+    return requests.get(x).json();
--- main.py
+++ main.py
@@ -2,3 +2,4 @@
 import python_modules
 print(python_modules.c_pow(8))
 print(python_modules.c_pow(9))
+print(python_modules.http_request("https://httpbin.org/ip"))
