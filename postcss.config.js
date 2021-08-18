00caab1fefd78b6e73dbd1f85a5915f533c4a23d
Riya <hiyoriorria@gmail.com>
2021-08-19 01:23:46 +0900

    微細な変更

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,16 @@
-module.exports = {
-    plugins: [
-        require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
-        require('postcss-import'),
-        require('postcss-flexbugs-fixes'),
-        require('postcss-preset-env')({
-            autoprefixer: {
-                flexbox: 'no-2009'
-            },
-            stage: 3
-        })
-    ]
-}
+// module.exports = {
+//     plugins: [
+//         require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
+//         require('postcss-import'),
+//         require('postcss-flexbugs-fixes'),
+//         require('postcss-preset-env')({
+//             autoprefixer: {
+//                 flexbox: 'no-2009'
+//             },
+//             stage: 3
+//         })
+//     ]
+// }
 
 // let environment = {
 //     plugins: [
@@ -47,3 +47,42 @@ module.exports = {
 // }
 
 // module.exports = environment
+const environment = ctx => ({
+    plugins: [
+        require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
+        require("postcss-import"),
+        require("postcss-flexbugs-fixes"),
+        require("postcss-preset-env")({
+            autoprefixer: {
+                flexbox: "no-2009"
+            },
+            stage: 3
+        }),
+        purgeCss(ctx)
+    ]
+});
+
+const purgeCss = ({ file }) => {
+    return require("@fullhuman/postcss-purgecss")({
+        content: htmlFilePatterns(file.basename),
+        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    });
+};
+
+const htmlFilePatterns = filename => {
+    switch (filename) {
+        case "main.scss":
+            return [
+                "./app/views/main/*.erb",
+            ];
+        default:
+            return [
+                "./app/**/*.html.erb",
+                "./config/initializers/simple_form_bootstrap.rb",
+                "./app/helpers/**/*.rb",
+                "./app/javascript/**/*.js"
+            ];
+    }
+};
+
+module.exports = ctx => environment(ctx);
