04beff9a69c4c7e03521dbee1363f380a7942ede
Riya <hiyoriorria@gmail.com>
2021-08-19 01:12:44 +0900

    微細な変更

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-let environment = {
+module.exports = {
     plugins: [
         require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
-        require('autoprefixer'),
         require('postcss-import'),
         require('postcss-flexbugs-fixes'),
         require('postcss-preset-env')({
@@ -9,27 +8,42 @@ let environment = {
                 flexbox: 'no-2009'
             },
             stage: 3
-        }),
+        })
     ]
 }
 
-// Only run PurgeCSS in production (you can also add staging here)
-if (process.env.RAILS_ENV === "production") {
-    environment.plugins.push(
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                './app/**/*.html.erb',
-                './app/helpers/**/*.rb',
-                './app/javascript/**/*.js',
-                './app/javascript/**/*.jsx',
-                './app/javascript/**/*.ts',
-                './app/javascript/**/*.tsx',
-                './app/javascript/**/*.vue',
+// let environment = {
+//     plugins: [
+//         require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
+//         require('autoprefixer'),
+//         require('postcss-import'),
+//         require('postcss-flexbugs-fixes'),
+//         require('postcss-preset-env')({
+//             autoprefixer: {
+//                 flexbox: 'no-2009'
+//             },
+//             stage: 3
+//         }),
+//     ]
+// }
 
-            ],
-            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-        })
-    )
-}
+// // Only run PurgeCSS in production (you can also add staging here)
+// if (process.env.RAILS_ENV === "production") {
+//     environment.plugins.push(
+//         require('@fullhuman/postcss-purgecss')({
+//             content: [
+//                 './app/**/*.html.erb',
+//                 './app/helpers/**/*.rb',
+//                 './app/javascript/**/*.js',
+//                 './app/javascript/**/*.jsx',
+//                 './app/javascript/**/*.ts',
+//                 './app/javascript/**/*.tsx',
+//                 './app/javascript/**/*.vue',
 
-module.exports = environment
+//             ],
+//             defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+//         })
+//     )
+// }
+
+// module.exports = environment
