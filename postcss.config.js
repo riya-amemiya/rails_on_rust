62392e07105635e83e339876cd3374ff5f75b847
Riya <hiyoriorria@gmail.com>
2021-08-19 13:05:54 +0900

    微細な変更

--- postcss.config.js
+++ postcss.config.js
@@ -12,83 +12,38 @@
 //     ]
 // }
 
-// let environment = {
-//     plugins: [
-//         require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
-//         require('autoprefixer'),
-//         require('postcss-import'),
-//         require('postcss-flexbugs-fixes'),
-//         require('postcss-preset-env')({
-//             autoprefixer: {
-//                 flexbox: 'no-2009'
-//             },
-//             stage: 3
-//         }),
-//     ]
-// }
-
-// // Only run PurgeCSS in production (you can also add staging here)
-// if (process.env.RAILS_ENV === "production") {
-//     environment.plugins.push(
-//         require('@fullhuman/postcss-purgecss')({
-//             content: [
-//                 './app/**/*.html.erb',
-//                 './app/helpers/**/*.rb',
-//                 './app/javascript/**/*.js',
-//                 './app/javascript/**/*.jsx',
-//                 './app/javascript/**/*.ts',
-//                 './app/javascript/**/*.tsx',
-//                 './app/javascript/**/*.vue',
-
-//             ],
-//             defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-//         })
-//     )
-// }
-
-// module.exports = environment
-const environment = ctx => ({
+let environment = {
     plugins: [
         require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
-        require("postcss-import"),
-        require("postcss-flexbugs-fixes"),
-        require("postcss-preset-env")({
+        require('autoprefixer'),
+        require('postcss-import'),
+        require('postcss-flexbugs-fixes'),
+        require('postcss-preset-env')({
             autoprefixer: {
-                flexbox: "no-2009"
+                flexbox: 'no-2009'
             },
             stage: 3
         }),
-        purgeCss(ctx)
     ]
-});
+}
 
-const purgeCss = ({ file }) => {
-    return require("@fullhuman/postcss-purgecss")({
-        content: htmlFilePatterns(file.basename),
-        defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    });
-};
+// Only run PurgeCSS in production (you can also add staging here)
+if (process.env.RAILS_ENV === "production") {
+    environment.plugins.push(
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                './app/**/*.html.erb',
+                './app/helpers/**/*.rb',
+                './app/javascript/**/*.js',
+                './app/javascript/**/*.jsx',
+                './app/javascript/**/*.ts',
+                './app/javascript/**/*.tsx',
+                './app/javascript/**/*.vue',
 
-const htmlFilePatterns = filename => {
-    switch (filename) {
-        case "main.scss":
-            console.log(filename);
-            return [
-                "./app/views/main/*.erb",
-                "./app/javascript/components/Hello.tsx",
-            ];
-        default:
-            console.log(filename);
-            return [
-                "./app/**/*.html.erb",
-                "./config/initializers/simple_form_bootstrap.rb",
-                "./app/helpers/**/*.rb",
-                "./app/javascript/**/*.js",
-                "./app/javascript/**/*.jsx",
-                "./app/javascript/**/*.ts",
-                "./app/javascript/**/*.tsx"
-            ];
-    }
-};
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
+    )
+}
 
-module.exports = ctx => environment(ctx);
+module.exports = environment
