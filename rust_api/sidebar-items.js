5a30620ed1b93db96368e4d2a9557314b804658c
riya81 <riya81@users.noreply.github.com>
2021-08-29 17:13:58 +0000

    deploy: 9568c8711ddd8ee8b3262620426586531ce75d4f

--- rust_api/sidebar-items.js
+++ rust_api/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["fact","xからyまでの階乗"],["gcd","xとyの最大公約数"],["k","ケルビン温度"],["lcm","xとyの最小公倍数"],["math",""],["os_name","os判定"],["pow","xの二乗"],["quotient_not_surplus","xとyの割り算の解(あまりなし)"],["random",""]]});
+initSidebarItems({"fn":[["fact","xからyまでの階乗"],["gcd","xとyの最大公約数"],["hello",""],["k","ケルビン温度"],["lcm","xとyの最小公倍数"],["math",""],["os_name","os判定"],["pow","xの二乗"],["quotient_not_surplus","xとyの割り算の解(あまりなし)"],["random",""]]});
