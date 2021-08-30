6d60d26134dcb88e6bc9d64fbae326bc39ab3960
riya81 <riya81@users.noreply.github.com>
2021-08-30 06:43:26 +0000

    deploy: 5b0b0372fa81667f96acdebb45c4a2aeaa1639b1

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["rust_api"] = {"name":"","dirs":[{"name":"include","files":["c_hello.rs","c_math.rs","c_random.rs"]},{"name":"os","files":["linux.rs","mod.rs"]}],"files":["include.rs","lib.rs"]};
+sourcesIndex["rust_api"] = {"name":"","dirs":[{"name":"include","files":["c_cpp.rs","c_hello.rs","c_math.rs","c_random.rs"]},{"name":"os","files":["linux.rs","mod.rs"]}],"files":["include.rs","lib.rs"]};
 createSourceSidebar();
